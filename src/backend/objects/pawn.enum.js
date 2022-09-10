e1366e4cda6b36b84744e278d0c1ff5b7abc320e
ogius <kowalewski.olgierd@gmail.com>
2022-09-10 11:43:08 +0200

    added colors

--- src/backend/objects/pawn.enum.js
+++ src/backend/objects/pawn.enum.js
@@ -3,6 +3,13 @@ const pawn = {
     yellow: 1,
     red: 2,
     green: 3,
-    black: 4
+    black: 4,
+    magenta: 5,
+    blue: 6,
+    brown: 7,
+    lightBlue: 8,
+    orange: 9,
+    pink: 10,
+    darkRed: 11
 };
 export default pawn;
