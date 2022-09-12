99deafc44f08a03a53581425c6bc47333325745b
ogius <kowalewski.olgierd@gmail.com>
2022-09-12 09:59:36 +0200

    added third and fourth player

--- src/backend/objects/board.js
+++ src/backend/objects/board.js
@@ -14,12 +14,24 @@ function CreateNewBoard(colors) {
     }
     for (let x = 0; x < newBoard.length; x++) {
         for (let y = 0; y < newBoard[x].length; y++) {
-            if (x + y <= 4 && x != 4 && y != 4) {
+            if (x + y <= 4 && x !== 4 && y !== 4) {
                 newBoard[x][y].content = colors[0];
                 newBoard[x][y].missing = colors[1];
-            } else if (x + y >= 26 && x != 11 && y != 11) {
+            } else if (x + y >= 26 && x !== 11 && y !== 11) {
                 newBoard[x][y].content = colors[1];
                 newBoard[x][y].missing = colors[0];
+            } else if (colors.length >= 2) {
+                if (x > 11 && y < 4) {
+                    if (!((x === 12 || x === 13) && (y === 3 || y === 2) && !(x !== 12 && y !== 3))) {
+                        newBoard[x][y].content = colors[2];
+                        newBoard[x][y].missing = colors[3];
+                    }
+                } else if (y > 11 && x < 4) {
+                    if (!((y === 12 || y === 13) && (x === 3 || x === 2) && !(y !== 12 && x !== 3))) {
+                        newBoard[x][y].content = colors[3];
+                        newBoard[x][y].missing = colors[2];
+                    }
+                }
             }
         }
     }
